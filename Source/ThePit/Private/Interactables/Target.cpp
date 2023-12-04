1c3015d81be4ebf7e951482b68500655f0f76d42
AlbertPangilinan <rayalbertz@gmail.com>
2023-12-04 18:28:33 -0500

    added target blueprint and c++ class

---
+++ Source/ThePit/Private/Interactables/Target.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactables/Target.h"
+
+// Niagara
+#include "NiagaraFunctionLibrary.h"
+
+
+// Sets default values
+ATarget::ATarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Mesh Setup
+	TargetMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TargetMesh"));
+	TargetMesh->bUseDefaultCollision = true;
+	//TargetMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	//TargetMesh->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
+	SetRootComponent(TargetMesh);
+}
+
+// Called every frame
+void ATarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called when the game starts or when spawned
+void ATarget::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ATarget::SpawnBulletImpactSystem()
+{
+	if (BulletImpactEffect) UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, BulletImpactEffect, GetActorLocation(), GetActorRotation());
+}
+
+void ATarget::KnockOver()
+{
+	SetActorRotation(GetActorRotation() + FRotator(0.f, 0.f, 90.f));
+}
---
+++ Source/ThePit/Public/Interactables/Target.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Target.generated.h"
+
+
+// Combat
+class UNiagaraSystem;
+
+
+UCLASS()
+class THEPIT_API ATarget : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ATarget(); // Sets default values for this actor's properties
+	virtual void Tick(float DeltaTime) override; // Called every frame
+
+
+protected:
+	virtual void BeginPlay() override; // Called when the game starts or when spawned
+
+
+	// Functions
+	// Interact
+	void SpawnBulletImpactSystem();
+	void KnockOver();
+
+
+	// Variables
+	// Components
+	UPROPERTY(EditAnywhere, Category = "Target Properties")
+	UStaticMeshComponent* TargetMesh;
+
+
+	UPROPERTY(EditAnywhere, Category = "Target Properties")
+	UNiagaraSystem* BulletImpactEffect;
+
+
+private:	
+
+};
