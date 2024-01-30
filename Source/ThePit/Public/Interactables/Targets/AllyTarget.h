a79a0fd798165b05350a1525eebd9bcf9993d5b1
AlbertPangilinan <rayalbertz@gmail.com>
2024-01-30 18:38:24 -0500

    added ally and enemy target classes and materials

---
+++ Source/ThePit/Public/Interactables/Targets/AllyTarget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactables/Target.h"
+#include "AllyTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEPIT_API AAllyTarget : public ATarget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ThePit/Public/Interactables/Targets/EnemyTarget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactables/Target.h"
+#include "EnemyTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEPIT_API AEnemyTarget : public ATarget
+{
+	GENERATED_BODY()
+	
+};
